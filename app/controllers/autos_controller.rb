e18822e3e187bc5abe4d72741917febd4bd89815
pcherian10 <pcherian10@gmail.com>
2018-07-19 05:27:22 -0700

    fixed garage paths

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -1,6 +1,27 @@
 class AutosController < ApplicationController
 
+  get "/garages" do
 
+  end
 
+  get "/garages/new" do
+
+  end
+
+  get "/garages/:id/edit" do
+
+  end
+
+  post "/garages/:id" do
+
+  end
+
+  get "/garages/:id" do
+
+  end
+
+  post "/garages" do
+
+  end
 
 end
