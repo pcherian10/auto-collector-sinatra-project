aa05a724ddbbe32aa181ff89f585a62493713773
pcherian10 <pcherian10@gmail.com>
2018-07-19 05:30:42 -0700

    added first garage, create garage and edit garage paths

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -1,15 +1,23 @@
 class AutosController < ApplicationController
 
   get "/garages" do
-
+    redirect_if_not_logged_in
+    @garages = Garage.all
+    erb :'garage/index'
   end
 
   get "/garages/new" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    erb :'garages/new'
 
   end
 
   get "/garages/:id/edit" do
-
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    @garage = Garage.find(parmas[:id])
+    erb :'garages/edit'
   end
 
   post "/garages/:id" do
