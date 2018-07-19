eaad7cb98329970c6196f80270bed665549f43bc
pcherian10 <pcherian10@gmail.com>
2018-07-19 05:39:59 -0700

    added post path for garage

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -16,12 +16,18 @@ class AutosController < ApplicationController
   get "/garages/:id/edit" do
     redirect_if_not_logged_in
     @error_message = params[:error]
-    @garage = Garage.find(parmas[:id])
+    @garage = Garage.find(params[:id])
     erb :'garages/edit'
   end
 
   post "/garages/:id" do
-
+    redirect_if_not_logged_in
+    @garage = Garage.find(params[:id])
+    unless Garage.valid_params?(params)
+      redirect "/garages/#{@garage.id}/edit?error=invalid garage"
+    end
+    @garage.update(params.select{|k|k=="name" || k=="capacity"})
+    redirect "/bags/#{@garage.id}"
   end
 
   get "/garages/:id" do
