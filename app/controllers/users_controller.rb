32c25c1f103a62cdfa0b6ff050a81466a4b4dab5
pcherian10 <pcherian10@gmail.com>
2018-07-20 07:49:25 -0700

    completed the users controller page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,11 +31,21 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-
+    @error_message = params[:error]
+    if !session[:user_id]
+      erb :'users/login'
+    else
+      redirect '/garages'
+    end
   end
 
   post '/login' do
-
+    user = User. find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user_id
+      redirect "/garages"
+    else
+      redirect to '/signup'
+    end
   end
-
 end
