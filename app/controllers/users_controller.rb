912c4d04b69f3c16913e66fa7b2578e2f99bbd12
pcherian10 <pcherian10@gmail.com>
2018-07-22 04:34:43 -0700

    added logout path in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,4 +49,14 @@ class UsersController < ApplicationController
       redirect to '/signup'
     end
   end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirecct to '/'
+    end
+  end
+
 end
